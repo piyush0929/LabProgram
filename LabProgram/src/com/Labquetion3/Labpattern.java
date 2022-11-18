52c084d3bfbc8434e646ced9c1e3f4f65c6e1979
hp <hp@LAPTOP-2P7KS5QD>
2022-11-18 13:14:48 +0530

    lab activity 17/11/2022

---
+++ LabProgram/src/com/Labquetion3/Labpattern.java
@@ -0,0 +1,18 @@
+package com.Labquetion3;
+
+public class Labpattern {
+
+	public static void main(String[] args) {
+		int number=1;
+		for(int i=0;i<=4;i++)
+		{
+			for(int j=0;j<=i;j++)
+			{
+				System.out.print(number++ +" ");
+			}
+			System.out.println();
+		}
+		
+	}
+
+
